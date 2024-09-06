26364b7d9560ab4a08819eb1e037550a79c55963
RizikH <rihaddad2@uncg.edu>
2024-09-06 10:16:15 -0400

    Significant visual changes and added pie.html to maringue pie page

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const profilePic = document.getElementById('profile-pic');
 
 window.addEventListener('scroll', function () {
   const scrollPosition = window.scrollY || document.documentElement.scrollTop;
-  const fadePoint = 600; // The point where the fade starts
+  const fadePoint = 650; // The point where the fade starts
 
   // Calculate the opacity based on the scroll position
   profilePic.style.opacity = Math.max(1 - scrollPosition / fadePoint, 0);
